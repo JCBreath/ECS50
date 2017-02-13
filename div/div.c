a26a4cd4c0d2e7aa4eb44c0979dc396e17895da6
JCBreath <Mimafama0@qq.com>
2017-02-12 21:38:43 -0500

    Add files via upload

--- div/div.c
+++ div/div.c
@@ -1,22 +1,29 @@
+/*	
+	Author: Siyuan yao
+	Date: 02/11/17
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int dd, ds; /*DiviDend and DiviSor*/
-
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-	int i, j;
-	dd = atoi(argv[1]);
-	ds = atoi(argv[2]);
-	
-	for(i=7; i>=0; i--)
-		if(dd & (1 << i))
-			break;
+	unsigned int dividend, divisor, quotient = 0, remainder = 0;
 
-	for(j=7; j>=0; j--)
-		if(ds & (1 << j))
-			break;
+	dividend = strtoul(argv[1], NULL, 0);
+	divisor = strtoul(argv[2], NULL, 0);
 
-	printf("%d\n", i + 1);
-	printf("%u\n", dd);
+    for(int i = 31; i >= 0; i--) /* 32-bit unsigned int */
+	{
+		remainder = remainder << 1; 	/* left opt (carry) */
+		if(dividend & (1<<i)) 			/* check if dividend is at front */
+			remainder++;				/* assign to remainder as part of dividend */
+		if(remainder >= divisor)		/* if remainder is larger than divisor */
+		{
+			remainder -= divisor;
+			quotient |= (1<<i);			/* change the specific bit of quotient to 1 */
+		}
+	}
+
+	printf("%u / %u = %u R %u\n", dividend, divisor, quotient, remainder);
 }
---
+++ div/divAssembly.s
@@ -0,0 +1,53 @@
+# Author: Siyuan Yao
+
+.global _start
+
+.data
+
+dividend:
+	.long 0
+
+divisor:
+	.long 0
+
+.text
+
+_start:
+	movl $0, %eax			# eax = quotient
+	movl $0, %edx			# edx = remainder
+	movl $31, %ecx			# ecx = i = 31
+	movl $1, %esi			# bit
+
+	for_loop_start:				# for(int i = 31; i >= 0; i--)
+		cmpl $0, %ecx			# i >= 0
+			jl for_loop_end		
+
+		shll $1, %edx			# remainder = remainder << 1; 
+
+		if_start1:				# if(dividend & (1<<i)) 
+			movl $1, %esi
+			shll %cl, %esi		# (1<<i)
+			andl dividend, %esi # dividend & (1<<i)
+			cmpl $0, %esi		# if()
+				je if_end1
+			addl $1, %edx		# remainder++;
+		if_end1:
+
+		if_start2:				# if(remainder >= divisor)
+			movl divisor, %ebx
+			cmpl %ebx, %edx		# remainder >= divisor
+				jl if_end2		# remainder < divisor
+			subl divisor, %edx	# remainder -= divisor;
+			movl $1, %esi		# 1
+			shll %cl, %esi		# 1<<i
+			orl %esi, %eax		# quotient |= (1<<i);
+		if_end2:
+
+		decl %ecx				# i--
+
+		jmp for_loop_start		# for
+
+	for_loop_end:
+
+done:
+	movl %eax, %eax
